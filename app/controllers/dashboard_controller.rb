69684fdedc25ceb4955055b55cd36bcb170fb983
lnieto <lnieto@Luiss-iMac.local>
2020-08-25 18:10:36 -0500

    use user location and get temperature data for farenheit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
+
+  require 'open-uri'
+  include DashboardHelper
+
   def index
-    @weather = Openweather2.get_weather(city: 'Sao Paulo,BR', units: 'imperial')
+    @countries = JSON.load( open("https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/6ee538beca8914133259b401ba47a550313e8984/countries.min.json") )
+    ip_address = request.remote_ip == '::1' ? '2806:104e:17:4ab1:bdf4:7fec:9687:8783' : request.remote_ip
+    location   = Geocoder.search(ip_address)#Geocoder.search('192.168.1.70') #? 
+    city       = location.first.city ? location.first.city : 'Sao Paulo,BR'
+    @weather   = x_get_city_weather(city)
   end
+
+
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,20 @@
 module DashboardHelper
+
+  def x_get_city_weather(city_name)
+    city_weather = Openweather2.get_weather(city: city_name, units: 'metric')
+    f_temperature = get_farenheit(city_weather)
+
+    {
+      city_weather: city_weather,
+      f_temperature: f_temperature
+    }
+  end 
+
+  def get_farenheit(city_weather)
+    {
+      min: '%2.f' % ( city_weather.min_temperature * 9/5.to_d + 32 ),
+      max: '%2.f' % ( city_weather.max_temperature * 9/5.to_d + 32 ),
+      temperature: '%2.f' % ( city_weather.temperature * 9/5.to_d + 32 )
+    }
+  end
 end
