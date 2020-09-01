29015281402c029c1af01a5ad1f1393dfef3b1a9
lnieto <lnieto@Luiss-iMac.local>
2020-09-01 11:44:38 -0500

    NoMethodError corrected

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,8 @@ class DashboardController < ApplicationController
   def index
     @cities    = JSON.parse( File.read('cities.json') ).uniq
     ip_address = request.remote_ip == '::1' ? '2806:104e:17:4ab1:bdf4:7fec:9687:8783' : request.remote_ip
-    location   = Geocoder.search(ip_address)#Geocoder.search('192.168.1.70') #? 
-    city       = location.first.city ? location.first.city : 'Sao Paulo,BR'
+    location   = Geocoder.search(ip_address)
+    city       = location.first ? location.first.city : 'Sao Paulo,BR'
     @weather   = get_city_weather(city)
   end
 
